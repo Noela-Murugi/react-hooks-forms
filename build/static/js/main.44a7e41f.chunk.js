71d7980793fb507f414ccc34c692a158256482a1
Noela-Murugi <murugin254@gmail.com>
2022-09-22 19:38:56 +0300

    completed the assignment

---
+++ build/static/js/main.44a7e41f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-forms"]=this["webpackJsonpreact-forms"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);var a=n(0),s=(n(1),n(3));var r=function(e){return Object(a.jsxs)("form",{children:[Object(a.jsx)("input",{type:"text",onChange:e.handleFirstNameChange,value:e.firstName}),Object(a.jsx)("input",{type:"text",onChange:e.handleLastNameChange,value:e.lastName}),Object(a.jsx)("button",{type:"submit",children:"Submit"})]})};n.n(s).a.render(Object(a.jsx)(r,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.44a7e41f.chunk.js.map
